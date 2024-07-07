3aa472dbb4be3e9706f1cabf1d522abfdc024f9a
naske <naske777ig@gmail.com>
2024-07-07 15:03:43 -0400

    feat: Add home page

--- src/app/(app)/page.js
+++ src/app/(app)/page.js
@@ -1,4 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <h1>Hello word</h1>;
+  return (
+    <main className="">
+      <nav className="bg-violet-800  p-4">
+        <div className="flex justify-between items-center">
+          <Link href="#home" className="font-semibold">
+            Home
+          </Link>
+          
+          <Link href="#request-uber" className="font-semibold">
+            Request Uber
+          </Link>
+          <Link href="#be-uber" className="font-semibold">
+            Be Uber
+          </Link>
+          <Link href="#home" className="font-semibold">
+            <Image width={30} height={30} src="user.svg" alt="user"/>
+          </Link>
+        </div>
+      </nav>
+      <div className="max-w-3xl mx-auto p-4">
+        <section id="home" className="text-center my-8">
+          <h1 className="text-3xl font-bold">Welcome to TGAweb</h1>
+          <p className="mt-4">
+            TGAweb is like Uber but operates on a subscription model, offering
+            better prices for both customers and drivers. Join us and experience
+            the new era of transportation.
+          </p>
+        </section>
+      </div>
+    </main>
+  );
 }
